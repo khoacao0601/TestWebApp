7f23cff565f2efb755584c750e6f762d1e1489f6
khoacao0601 <khoacao0601@gmail.com>
2020-04-10 23:17:29 -0700

    Dev v1.0

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -4,22 +4,27 @@ export default class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      message: null,
-      isLoading: true
+      question: null
     };
   }
 
   componentDidMount() {
-    fetch('/api/health-check')
+    fetch('/api/questions')
       .then(res => res.json())
-      .then(data => this.setState({ message: data.message || data.error }))
-      .catch(err => this.setState({ message: err.message }))
-      .finally(() => this.setState({ isLoading: false }));
+      .then(data => {
+        this.setState({ question: data });
+        // eslint-disable-next-line no-console
+        console.log(this.state.question[0].content);
+      })
+      .catch(err => this.setState({ message: err.message }));
   }
 
   render() {
-    return this.state.isLoading
-      ? <h1>Testing connections...</h1>
-      : <h1>{ this.state.message.toUpperCase() }</h1>;
+    return (
+      <div>
+        <p>Question: </p>
+        <div>{this.state.question[0].content}</div>
+      </div>
+    );
   }
 }
