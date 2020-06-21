857e8a75a9e697309e81258169f650191fd2631a
khoacao0601 <khoacao0601@gmail.com>
2020-06-21 01:43:16 -0700

    Update 2:00AM 6/21/2020

--- client/components/answers.jsx
+++ client/components/answers.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { checkEmptyAnswer } from '../reduxActions';
+import { checkEmptyAnswer, checkEmptyAnswerMinus } from '../reduxActions';
 
 class Answers extends React.Component {
   constructor(props) {
@@ -17,16 +17,31 @@ class Answers extends React.Component {
     this.setState({ answerArray: this.props.content[this.props.questionIndex].array_agg });
   }
 
-  dispatchCheckEmpty() {
-    var x = document.getElementById('sendValue').defaultChecked;
+  checkConditionforEmptyAnswer() {
     var { dispatch } = this.props;
-    dispatch(checkEmptyAnswer(x));
+    var x = this.props.arrayAnswers.indexOf(this.props.content);
+    var index = this.props.answerStatus.indexOf(x);
+
+    var checkPoint = this.props.answerStatus.filter(function (number) {
+      return number === x;
+    });
+
+    if (checkPoint.length === 0) {
+      this.dispatchCheckEmpty(x);
+    } else {
+      dispatch(checkEmptyAnswerMinus(index));
+    }
+  }
+
+  dispatchCheckEmpty(value) {
+    var { dispatch } = this.props;
+    dispatch(checkEmptyAnswer(value));
   }
 
   rendAnswer() {
     return (
       <div className="form-check">
-        <input className="form-check-input" id="sendValue" type="checkbox" onChange={this.dispatchCheckEmpty.bind(this)}/>
+        <input className="form-check-input" type="checkbox" onClick={this.checkConditionforEmptyAnswer.bind(this)}/>
         <label className="form-check-label" htmlFor={this.props.content}>{this.props.content}</label>
         <br/>
       </div>
--- client/reducer/answerReducer.jsx
+++ client/reducer/answerReducer.jsx
@@ -2,6 +2,10 @@ var answerStatus = (state = [], action) => {
   switch (action.type) {
     case 'addStatus':
       return [...state, action.item];
+    case 'remove':
+      return state.filter((e, i) => i !== action.index);
+    case 'reset':
+      return [...state.slice(0, length)];
     default:
       return state;
   }
