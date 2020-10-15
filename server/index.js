753918082240b0c7128a8406301c0c4b3757d0f6
Khoa Cao <khoacao0601@gmail.com>
2020-10-14 23:30:53 -0700

    Update index.js

--- server/index.js
+++ server/index.js
@@ -6,12 +6,7 @@ const ClientError = require('./client-error');
 const staticMiddleware = require('./static-middleware');
 const sessionMiddleware = require('./session-middleware');
 
-const app = express();
 
-app.use(staticMiddleware);
-app.use(sessionMiddleware);
-
-app.use(express.json());
 
 app.get('/api/health-check', (req, res, next) => {
   db.query('select \'successfully connected\' as "message"')
