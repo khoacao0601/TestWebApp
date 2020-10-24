a3dd061a6ac815611c9122dacba7a31cfaaa62b2
Khoa Cao <khoacao0601@gmail.com>
2020-10-23 23:19:26 -0700

    Update changeDirection.jsx

--- client/reducer/changeDirection.jsx
+++ client/reducer/changeDirection.jsx
@@ -1,15 +1,4 @@
-var changeDirection = (state = 'amountQuestion', action) => {
-  switch (action.type) {
-    case 'amountQuestion':
-      /* eslint-disable */
-      return state = 'amountQuestion';
-    case 'questionList':
-      /* eslint-disable */
-      return state = 'questionList';
-    default:
-      return state;
-  }
-  0123456789
+
 };
 
 module.exports = changeDirection;
