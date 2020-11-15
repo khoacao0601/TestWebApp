8469fef308cb708506483798f81a9c2fdc84d3bc
Khoa Cao <khoacao0601@gmail.com>
2020-11-14 23:56:47 -0800

    Update index.js

--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@ const db = require('./database');
 const ClientError = require('./client-error');
 const staticMiddleware = require('./static-middleware');
 const sessionMiddleware = require('./session-middleware');
-0123456789
+
 const app = express();
 
 app.use(staticMiddleware);
