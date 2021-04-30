0361e8def92ed1e1c55e9484f4d56570fcfaca4a
Khoa Cao <khoacao0601@gmail.com>
2021-04-29 18:51:04 -0700

    Update answers.jsx

--- client/components/answers.jsx
+++ client/components/answers.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { checkEmptyAnswer, checkEmptyAnswerMinus } from '../reduxActions';
-
+012345
 class Answers extends React.Component {
   constructor(props) {
     super(props);
